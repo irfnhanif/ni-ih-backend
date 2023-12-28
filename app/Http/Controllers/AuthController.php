ee6f85e56686ce9acc6e546ae5bed7e20ab848ce
Irfan Hanif <14.irfanh@gmail.com>
2023-12-28 12:57:12 +0700

    created register and login functions

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,5 +7,42 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:100',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+        try {
+            $user = User::create($validatedData);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Server error'], 500);
+        }
+        
+        return response()->json(['message' => 'User created', 'user' => $user]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+        try {
+            $accessToken = $user->createToken('authToken')->plainTextToken;
+        } catch (\Exception) {
+            return response()->json(['error' => 'Server error'], 500);
+        }
+
+        return response()->json(['message' => 'User authenticated', 'token' => $accessToken]);
+    }
 }
