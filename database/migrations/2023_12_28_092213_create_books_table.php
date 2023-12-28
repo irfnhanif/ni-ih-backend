d40aa6cf5de68c6e1411bf278bb5a45688731e27
Irfan Hanif <14.irfanh@gmail.com>
2023-12-28 16:32:56 +0700

    created migration files and updated api route

---
+++ database/migrations/2023_12_28_092213_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('isbn');
+            $table->string('title');
+            $table->string('subtitle')->nullable();
+            $table->string('author')->nullable();
+            $table->dateTime('published')->nullable();
+            $table->string('publisher')->nullable();
+            $table->integer('pages')->nullable();
+            $table->text('description')->nullable();
+            $table->string('website')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -14,21 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/register', 'App\Http\Controllers\AuthController@register');
 Route::post('/login', 'App\Http\Controllers\AuthController@login');
 
-Route::prefix('user')->group(function () {
+Route::middleware('auth:sanctum')->prefix('user')->group(function () {
     Route::get('/', 'App\Http\Controllers\AuthController@user');
     Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
 });
 
-Route::prefix('books')->group(function () {
+Route::middleware('auth:sanctum')->prefix('books')->group(function () {
     Route::get('/', 'App\Http\Controllers\BookController@index');
-    Route::post('/', 'App\Http\Controllers\BookController@store');
+    Route::post('/add', 'App\Http\Controllers\BookController@store');
     Route::get('/{id}', 'App\Http\Controllers\BookController@show');
     Route::put('/{id}', 'App\Http\Controllers\BookController@update');
     Route::delete('/{id}', 'App\Http\Controllers\BookController@destroy');
