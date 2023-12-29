8efeaf0169844a46f0b91f061b4750971469b3d8
Irfan Hanif <14.irfanh@gmail.com>
2023-12-29 10:18:14 +0700

    updated auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,60 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
+use App\Models\User; 
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|max:100',
-            'email' => 'email|required|unique:users',
-            'password' => 'required|confirmed'
-        ]);
-
-        $validatedData['password'] = bcrypt($request->password);
-
         try {
+            $validatedData = $request->validate([
+                'name' => 'required|max:100',
+                'email' => 'email|required|unique:users',
+                'password' => 'required|confirmed'
+            ]);
+
+            $validatedData['password'] = bcrypt($request->password);
+
             $user = User::create($validatedData);
-            $user->sendEmailVerificationNotification();
+            
+            return response()->json(['message' => 'User created', 'user' => $user->makeHidden('password')]);
         } catch (\Exception) {
             return response()->json(['message' => 'Server error'], 500);
         }
-        
-        return response()->json(['message' => 'User created', 'user' => $user->makeHidden('password')]);
     }
 
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'email|required',
-            'password' => 'required'
-        ]);
-
-        if (!auth()->attempt($request->only('email', 'password'))) {
-            return response()->json(['message' => 'Wrong email or password'], 401);
-        }
-
-        $user = User::where('email', $request->email)->first();
         try {
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'required'
+            ]);
+
+            if (!auth()->attempt($request->only('email', 'password'))) {
+                return response()->json(['message' => 'Wrong email or password'], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
             $accessToken = $user->createToken('authToken')->plainTextToken;
-        } catch (\Exception) {
+
+            return response()->json(['message' => 'Logged in', 'token' => $accessToken]);
+        } catch (\Exception $e) {
             return response()->json(['message' => 'Server error'], 500);
         }
-
-        return response()->json(['message' => 'Logged in', 'token' => $accessToken]);
     }
 
     public function user()
     {
         try {
             $user = User::where('id', auth()->id())->first();
+
+            return response()->json(['user' => $user]);
         } catch (\Exception) {
             return response()->json(['message' => 'Server error'], 500);
         }
-
-        return response()->json(['user' => $user]);
     }
 
     public function logout()
@@ -64,10 +64,10 @@ class AuthController extends Controller
         try {
             $user = User::where('id', auth()->id())->first();
             $user->tokens()->delete();
+
+            return response()->json(['message' => 'Logged out']);
         } catch (\Exception) {
             return response()->json(['message' => 'Server error'], 500);
         }
-
-        return response()->json(['message' => 'Logged out']);
     }
 }
