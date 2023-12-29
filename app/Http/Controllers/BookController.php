77e3ee96ab737ee47c2080eddd91abd0c86e2717
Irfan Hanif <14.irfanh@gmail.com>
2023-12-29 07:28:39 +0700

    added show and update functions

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -16,7 +16,7 @@ class BookController extends Controller
             $books = Book::where('user_id', auth()->id())->paginate(10);
             return response()->json(['books' => $books]);
         } catch (\Exception) {
-            return response()->json(['error' => 'Server error'], 500);
+            return response()->json(['message' => 'Server error'], 500);
         }
     }
 
@@ -43,25 +43,62 @@ class BookController extends Controller
             $book = Book::create($validatedData);
             return response()->json(['message' => 'Book created', 'book' => $book]);
         } catch (\Exception) {
-            return response()->json(['error' => 'Server error'], 500);
+            return response()->json(['message' => 'Server error'], 500);
         }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Book $book)
+    public function show(Request $request)
     {
-        //
+        $book = Book::where('id', (int)$request->book_id)->first();
+
+        if (!$book) {
+            return response()->json(['message' => 'Book not found'], 404);
+        }
+
+        if ($book->user_id !== auth()->id()) {
+            return response()->json(['message' => 'Forbidden to access book'], 403);
+        }
+
+        return response()->json(['book' => $book]);
     }
 
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Book $book)
+    public function update(Request $request)
     {
-        //
+        $book = Book::where('id', (int)$request->book_id)->first();
+        
+        if (!$book) {
+            return response()->json(['message' => 'Book not found'], 404);
+        }
+
+        if ($book->user_id !== auth()->id()) {
+            return response()->json(['message' => 'Forbidden to access book'], 403);
+        }
+
+        $validatedData = $request->validate([
+            'isbn' => 'required|string|unique:books,isbn,' . $book->id,
+            'title' => 'required|string|max:255',
+            'subtitle' => 'string|nullable',
+            'author' => 'string|nullable',
+            'published' => 'datetime|nullable',
+            'publisher' => 'string|nullable',
+            'pages' => 'int|nullable',
+            'description' => 'string|nullable',
+            'website' => 'string|nullable',
+        ]);
+
+        try {
+            $book->update($validatedData);
+            return response()->json(['message' => 'Book updated', 'book' => $book]);
+        } catch (\Exception) {
+            return response()->json(['message' => 'Server error'], 500);
+        }
     }
 
     /**
